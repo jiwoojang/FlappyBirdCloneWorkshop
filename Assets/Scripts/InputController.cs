a2ecbfbaff8ef8574b4889dded184cb5fc3d361a
Jiwoo Jang <jiwooo.jang@gmail.com>
2019-08-10 13:48:55 -0400

    Upgraded project to 2019.2.0f1 and fixed bug in mobile input. Closes #1 and #3

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -15,15 +15,26 @@ public class InputController : MonoBehaviour
     // This function will change behaviour depepdning on platform
     private bool IsFlapKeyPressed() {
 #if UNITY_IOS || UNITY_ANDROID
-        return Input.touchCount > 0;
+        return GetTouchInput();
 #else
         if (UnityEditor.EditorApplication.isRemoteConnected) {
-            return Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+            return GetTouchInput();
         }
         else {
-            return Input.GetKeyDown(flapKey);
+            return GetDesktopInput();
         }
 #endif
+    }
+
+    // For touch inputs
+    // Mobile and debug Unity Remote
+    private bool GetTouchInput() {
+        return Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+    }
+
+    // For regular keycap input
+    private bool GetDesktopInput() {
+        return Input.GetKeyDown(flapKey);
     }
 
     private void Update() {
